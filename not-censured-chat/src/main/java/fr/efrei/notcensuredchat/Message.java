8b23758c1ea9f6635ac64ef702d2aa683013ddfb
petit-chat <76547570+petit-chat@users.noreply.github.com>
2022-01-05 10:12:32 +0100

    updated content filtering

--- not-censured-chat/src/main/java/fr/efrei/notcensuredchat/Message.java
+++ not-censured-chat/src/main/java/fr/efrei/notcensuredchat/Message.java
@@ -1,25 +1,47 @@
 package fr.efrei.notcensuredchat;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Message {
+    private final ArrayList<String> badWords = new ArrayList<>(List.of("fuck"));
+
     private String author;
     private String content;
     private Date date;
 
-    public String getAuthor() {return author;}
-    public void setAuthor(String author) {this.author = author;}
-    public String getContent() {return content;}
-    public void setContent(String content) {this.content = content;}
-    public Date getDate() {return date;}
-    public void setDate(Date date) {this.date = date;}
-
     @Override
     public String toString() {
-        return "Tset :Message{" +
-                "author='" + author + '\'' +
-                ", content='" + content + '\'' +
-                ", date=" + date +
-                '}';
+        return "{ author='" + author + "', content='" + content + "', date='" + date + "'}";
+    }
+
+    public void censureMessage() {
+        boolean found = false;
+
+        for (String badWord : badWords) {
+            if (content.contains(badWord)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) return;
+
+        StringBuilder censuredContent = new StringBuilder();
+
+        for (String word : content.split("\\s+")) {
+            for (String badWord : badWords) {
+                censuredContent.append(" ");
+
+                if (word.contains(badWord)) {
+                    censuredContent.append("*".repeat(word.length()));
+                } else {
+                    censuredContent.append(word);
+                }
+            }
+        }
+
+        content = censuredContent.toString();
     }
 }
