5a36c2749d2cc6c2a87e1798c31e12952441865b
petit-chat <76547570+petit-chat@users.noreply.github.com>
2022-01-07 23:57:30 +0100

    chat finally working

--- chat-ws-api/src/app.service.ts
+++ chat-ws-api/src/app.service.ts
@@ -1,27 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { Message } from './message';
 import * as fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 const basePath = '../not-censured-chat';
-const inFilePath = 'dataIn/message.json';
-const outFilePath = 'dataOut/message.json';
+const dataInPath = 'dataIn';
+const dataOutPath = 'dataOut';
 
 @Injectable()
 export class AppService {
   async sendMessage(message: Message): Promise<Message> {
-    this.writeMessageToFile(message);
-    await new Promise((_) => setTimeout(_, 500));
-    return this.readMessageFromFile();
+    const uuid = uuidv4();
+    this.writeMessageToFile(message, uuid);
+    await new Promise((_) => setTimeout(_, 1000)); // waiting for spring integration to proceed
+    return this.readMessageFromFile(uuid);
   }
 
-  writeMessageToFile(message: Message) {
-    fs.writeFileSync(`${basePath}/${inFilePath}`, JSON.stringify(message), {
-      flag: 'w',
-    });
+  writeMessageToFile(message: Message, uuid: string) {
+    fs.writeFileSync(
+      `${basePath}/${dataInPath}/${uuid}.json`,
+      JSON.stringify(message),
+      {
+        flag: 'w',
+      },
+    );
   }
 
-  readMessageFromFile(): Message {
-    const message = fs.readFileSync(`${basePath}/${outFilePath}`);
+  readMessageFromFile(uuid: string): Message {
+    const filePath = `${basePath}/${dataOutPath}/${uuid}.json`;
+    const message = fs.readFileSync(filePath);
+    fs.unlinkSync(filePath);
     return JSON.parse(message.toString());
   }
 }
